8e612d0c22bf848fdab47ecb587100ff0e163f53
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-10 12:33:45 +0200

    Ejercicio 3 - Uso de variables globales

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 
+int g = 20;
+
 int suma(int a, int b) {
     return a + b;
 }
@@ -14,6 +16,10 @@ int main() {
     std::cout << "Hola Mundo" << std::endl;
 
     std::cout << "La suma es: " << suma(5, 3) << std::endl;
+
+    std::cout << "Valor de g antes de la función suma: " << g << std::endl;
+    std::cout << "La suma es: " << suma(5, g) << std::endl;
+    std::cout << "Valor de g después de la función suma: " << g << std::endl;
 
     return 0;
 }
