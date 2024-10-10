c5bdf7d5b218d28a9a651924c5b81b4a34e9f47d
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-10 12:00:56 +0200

    Ejericio 1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by pinchiguillo on 10/10/2024.
+//
+
+#include "main.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo" << std::endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by pinchiguillo on 10/10/2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
