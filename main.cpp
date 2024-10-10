ac057bd9399f6b3f99c8f90f84cc44a83a6787c7
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-10 12:48:05 +0200

    Ejercicio 7 - Preprocesador

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 #include "main.h"
 
 #include <iostream>
+#include "funciones.h"
 
 int g = 20;
 
-int suma(int a, int b) {
-    return a + b;
-}
+#define PI 3.14159
+
+#define AREA_CIRCULO(r) (PI * (r) * (r))
 
 int main() {
     std::cout << "Hola Mundo" << std::endl;
@@ -20,6 +21,12 @@ int main() {
     std::cout << "Valor de g antes de la función suma: " << g << std::endl;
     std::cout << "La suma es: " << suma(5, g) << std::endl;
     std::cout << "Valor de g después de la función suma: " << g << std::endl;
+
+    std::cout << "Suma: " << suma(5, g) << std::endl;
+    std::cout << "Resta: " << resta(5, g) << std::endl;
+
+    std::cout << "Área del círculo: " << AREA_CIRCULO(5) << std::endl;
+    std::cout << "PI: " << PI << std::endl;
 
     return 0;
 }
