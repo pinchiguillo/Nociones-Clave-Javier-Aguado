4a040d5892dcfbb3cd491d18ac80c27446098a49
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-10 12:33:06 +0200

    Ejercicio 2 - Función de suma

--- main.cpp
+++ main.cpp
@@ -6,7 +6,14 @@
 
 #include <iostream>
 
+int suma(int a, int b) {
+    return a + b;
+}
+
 int main() {
     std::cout << "Hola Mundo" << std::endl;
+
+    std::cout << "La suma es: " << suma(5, 3) << std::endl;
+
     return 0;
 }
