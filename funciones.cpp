b0450f96eea54b2cbb28789e191e68512d62754d
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-10 12:46:49 +0200

    Ejercicio 6 - Organización de un programa C++

---
+++ funciones.cpp
@@ -0,0 +1,10 @@
+// funciones.cpp
+#include "funciones.h"  // Incluimos el archivo de cabecera
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int resta(int a, int b) {
+    return a - b;
+}
---
+++ funciones.h
@@ -0,0 +1,9 @@
+// funciones.h
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+// Declaración de las funciones
+int suma(int a, int b);
+int resta(int a, int b);
+
+#endif  // FUNCIONES_H
