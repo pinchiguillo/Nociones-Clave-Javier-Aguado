1ba5d8fec93c0b708da39fa6133a8bb4af9c75e8
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-12 19:42:06 +0200

    Ejercicio 3 - Remake | Reestructurado el proyecto y añadido "proyecto secundario"

---
+++ Proyecto 2/main2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pinchiguillo on 12/10/2024.
+//
+
+#include "main2.h"
+#include <iostream>
+
+int main() {
+    std::cout << "¡Hola CLion!" << std::endl;
+
+
+    return 0;
+}
---
+++ Proyecto 2/main2.h
@@ -0,0 +1,16 @@
+//
+// Created by pinchiguillo on 12/10/2024.
+//
+
+#ifndef MAIN2_H
+#define MAIN2_H
+
+
+
+class main2 {
+
+};
+
+
+
+#endif //MAIN2_H
