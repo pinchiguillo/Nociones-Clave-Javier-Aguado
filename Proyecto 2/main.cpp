e73b10497be99e8a28287038ad4b24baca1746b4
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-16 10:58:15 +0200

    REMAKE - Ejercicio 5

--- Proyecto 2/main.cpp
+++ Proyecto 2/main.cpp
@@ -7,12 +7,17 @@
 #include <fmt/core.h>
 #include "funciones.h"
 
+#define PI 3.14159
+#define AREA_CIRCULO(r) (PI * (r) * (r))
+
 int main() {
     std::cout << "¡Hola CLion!" << std::endl;
 
     fmt::print("¡Hola CLion!\n");
 
     fmt::print("Suma: {}\n", suma(5, 3));
+
+    fmt::print("Area del circulo: {}\n", AREA_CIRCULO(5));
 
     return 0;
 }
