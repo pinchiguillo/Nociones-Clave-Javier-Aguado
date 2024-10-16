11fba97fcea41d52b6ef69bdb9db45d3d79dedd2
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-10-16 10:29:21 +0200

    REMAKE - Ejercicio 1

--- Proyecto 1/main.cpp
+++ Proyecto 1/main.cpp
@@ -3,15 +3,13 @@
 //
 
 #include "main.h"
-#include <fmt/core.h>
 #include <iostream>
-#include "funciones.h"
 
 int g = 20;
 
-#define PI 3.14159
-
-#define AREA_CIRCULO(r) (PI * (r) * (r))
+int suma(int a, int b) {
+    return a + b;
+}
 
 int main() {
     std::cout << "Hola Mundo" << std::endl;
@@ -21,16 +19,6 @@ int main() {
     std::cout << "Valor de g antes de la función suma: " << g << std::endl;
     std::cout << "La suma es: " << suma(5, g) << std::endl;
     std::cout << "Valor de g después de la función suma: " << g << std::endl;
-
-    std::cout << "Suma: " << suma(5, g) << std::endl;
-    std::cout << "Resta: " << resta(5, g) << std::endl;
-
-    std::cout << "Área del círculo: " << AREA_CIRCULO(5) << std::endl;
-    std::cout << "PI: " << PI << std::endl;
-
-    fmt::print("Using fmt library\n");
-    fmt::print("La suma de 5 y 3 es: {}\n", suma(5, 3));
-    fmt::print("La resta de 5 y 3 es: {}\n", resta(5, 3));
 
     return 0;
 }
